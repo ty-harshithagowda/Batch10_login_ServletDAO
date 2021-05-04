455320f10addb8670c0bfc45a144a84b86016d68
ty-harshithagowda <harshitha.br@testyantra.in>
2021-05-04 20:09:36 +0530

    added servletdao assignment

---
+++ src/com/te/servlet/dao/DBConnection.java
@@ -0,0 +1,13 @@
+package com.te.servlet.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	protected static Connection initialize() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/login?user=root&password=root@123");
+		return con;
+	}
+
+}
